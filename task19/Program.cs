f338d8bf10e94de29b102f98e2d88349523fdf04
MalaFerre <malaferree@outlook.com>
2023-02-19 12:52:42 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число: ");
+string? number = (Console.ReadLine());
+if (number!.Length < 5 || number!.Length > 5)
+{
+    System.Console.WriteLine("Некорректное число! ");
+}
+else if (number[1] == number[3] && number[0] == number[4])
+{
+    System.Console.WriteLine($"Число полиндром!");
+}
+else
+{
+    System.Console.WriteLine("Число не полиндром!");
+}
